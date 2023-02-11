3f383697234da068947bfdda451b992d6d3b9696
simplejain309 <66191888+simplejain309@users.noreply.github.com>
2023-02-11 18:07:44 +0530

    Update settings.py

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -141,7 +141,7 @@ NOTIFY_UPDATE_TIME_INTERVAL = 2000
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 #VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
